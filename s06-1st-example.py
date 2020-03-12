64392eabce3144f6b6085fb8daf24ea0a37e0653
Sincab <sinan.selcuk@hotmail.com>
2020-03-12 19:53:23 +0000

    mod

--- s06-1st-example.py
+++ s06-1st-example.py
@@ -2,14 +2,15 @@ from random import randint
 
 def main():
     # Open file for writing data
-    outfile = open("Numbers.txt", "w")
+    outfile = open("Numbers.csv", "w")
     for i in range(10):
-        outfile.write(str(randint(0, 9)) + " ")
+        outfile.write(str(randint(0, 9)) + ",") #if coma in
+    outfile.write(str(randint(0, 9)))
     outfile.close() # Close the file
     # Open file for reading data
-    infile = open("Numbers.txt", "r")
+    infile = open("Numbers.csv", "r")
     s = infile.read()
-    numbers = [int(x) for x in s.split()]
+    numbers = [int(x) for x in s.split(",")]
     for number in numbers:
         print(number, end = " ")
     infile.close() # Close the file
